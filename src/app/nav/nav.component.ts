1768e034e4e7e9d6b109fc660d3d86c0adad7fd4
hari <tejahari666666@gmail.com>
2024-10-24 06:58:23 +0530

    added all the files in project

---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent {
+  name="hari";
+  img={
+    src:"https://msatechnosoft.in/blog/wp-content/uploads/2018/06/bootstrap-image-MSA-Technosoft.jpg",
+    alt:"image"
+  }
+
+}
